e1c5846989521fdf64347640b33049e0447757d0
johanfortus <fortusjohan@gmail.com>
2026-03-01 23:18:40 -0500

    use lb for UI and convert to kg in calculations

--- lib/services/workout_service.dart
+++ lib/services/workout_service.dart
@@ -153,11 +153,9 @@ class WorkoutService {
 
     double perMin;
     if (gender == 'female') {
-      perMin = (0.4472 * avgHr - 0.1263 * weight + 0.074 * age - 20.4022) /
-          4.184;
+      perMin = ((0.4472 * avgHr) - (0.1263 * (weight * 0.45359237)) + (0.074 * age) - 20.4022) / 4.184;
     } else {
-      perMin = (0.6309 * avgHr + 0.1988 * weight + 0.2017 * age - 55.0969) /
-          4.184;
+      perMin = ((0.6309 * avgHr) + (0.1988 * (weight * 0.45359237)) + (0.2017 * age) - 55.0969) / 4.184;
     }
 
     if (perMin < 0) perMin = 0;
