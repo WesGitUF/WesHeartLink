89f387c09e72cf316c74465bc6fea77bc49d98bb
johanfortus <fortusjohan@gmail.com>
2026-03-12 04:07:06 -0400

    allow profile sound preview to play with music

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -67,7 +67,7 @@ class _ProfileScreenState extends State<ProfileScreen> with WidgetsBindingObserv
         android: AudioContextAndroid(
           contentType: AndroidContentType.sonification,
           usageType: AndroidUsageType.assistanceSonification,
-          audioFocus: AndroidAudioFocus.none,
+          audioFocus: AndroidAudioFocus.gainTransientMayDuck,
         ),
       ),
     );
