a2f23bb51c745974d40206402efbc23b79d476a7
johanfortus <fortusjohan@gmail.com>
2026-03-12 22:56:56 -0400

    add app color palette constants from figma

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-	AppColors._();
+  AppColors._();
 
-	static const Color placeholder = Colors.black;
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color background = black;
+  static const Color surfacePrimary = Color(0xFF17191C);
+  static const Color surfaceSecondary = Color(0xFF030712);
+
+  static const Color textPrimary = white;
+  static const Color textSecondary = Color(0xB3FFFFFF);
+  static const Color textTertiary = Color(0x99FFFFFF);
+  static const Color textMuted = Color(0x66FFFFFF);
+  static const Color textFaint = Color(0x0DFFFFFF);
+
+  static const Color iconDefault = Color(0xFF6B7280);
+  static const Color strokeSubtle = Color(0x1AFFFFFF);
+  static const Color strokeSoft = Color(0x0DFFFFFF);
+
+  static const Color blue = Color(0xFF51A2FF);
+  static const Color blueSoft = Color(0x332B7FFF);
+  static const Color purple = Color(0xFFC27AFF);
+  static const Color purpleStrong = Color(0xFFAD46FF);
+  static const Color green = Color(0xFF05DF72);
+  static const Color greenStrong = Color(0xFF00C950);
+  static const Color orange = Color(0xFFFF8904);
+  static const Color orangeStrong = Color(0xFFFF6900);
+  static const Color red = Color(0xFFEF4444);
+  static const Color redStrong = Color(0xFFFB2C36);
+  static const Color pink = Color(0xFFDC269C);
+  static const Color coral = Color(0x26FF6467);
+
+  static const Color cardOverlayStrong = Color(0xCC17191C);
+  static const Color cardOverlaySoft = Color(0x9917191C);
+  static const Color navOverlay = Color(0x6617191C);
 }
