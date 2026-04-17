14d3d4fcfd1a39e559bf85cafcb0a10b3fb4fd0e
maria <mjsel.reynolds@ufl.edu>
2026-04-17 07:23:20 -0400

    Reverted back to splash screen commit and fixed average HR outline error

--- lib/radial-gauge.dart
+++ lib/radial-gauge.dart
@@ -1695,10 +1695,10 @@ class _GaugeChartState extends State<GaugeChart>
                                 borderRadius: BorderRadius.circular(20),
                                 border: Border.all(
                                   color:
-                                  _maxHeartRate != null && _maxSessionHR > 0
+                                  _maxHeartRate != null && averageHR > 0
                                       ? _colorForZone(
                                     getZoneForHR(
-                                      _maxSessionHR,
+                                      averageHR.round(),
                                       _maxHeartRate!,
                                     ),
                                   )
